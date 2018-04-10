c8ddf5b941505dbcda725efe99e8ce9b8559a32d
Nicolas Manrique <nicolasmn97@outlook.com>
2018-04-10 15:47:22 -0500

    prueba- se sube primer archivo

---
+++ NM_linAlg.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <math.h>
+
+
+int main(){
+
+    //Vector de 10 componentes
+    double *a = new double[10];
+    
+    double **mat = new double*[4];
+    int i;
+    for ( i=0; i<4; i++){
+
+        mat[i] = new double[5];
+
+    }
+
+}
